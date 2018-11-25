9a8116da1e3ff86ff6638657b2b432ae84bda716
Erwan Le Tutour <erwanletutour.elt@gmail.com>
2018-11-25 11:28:03 +0100

    initial
    commit

---
+++ src/main/java/com/myApp/weather/Utils/ApiUtils.java
@@ -0,0 +1,6 @@
+package com.myApp.weather.Utils;
+
+public class ApiUtils {
+
+    private static final String DARK_SKY_KEY = "349b49115a426ace2e143b5e92a262e6";
+}
---
+++ src/main/java/com/myApp/weather/controller/WeatherController.java
@@ -0,0 +1,18 @@
+package com.myApp.weather.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WeatherController {
+
+    @GetMapping("/")
+    public String index() {
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "/home";
+    }
+}
