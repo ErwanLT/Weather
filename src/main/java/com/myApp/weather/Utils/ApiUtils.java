0bf16cefa0dbb634a77aac48aa1f9c54efa03873
Erwan Le Tutour <erwanletutour.elt@gmail.com>
2019-01-31 08:35:37 +0100

    Update ApiUtils.java
    refactor TODO, the API key have been stored in environment variable

--- src/main/java/com/myApp/weather/Utils/ApiUtils.java
+++ src/main/java/com/myApp/weather/Utils/ApiUtils.java
@@ -23,7 +23,6 @@ package com.myApp.weather.utils;
 
 public class ApiUtils {
 
-    //TODO when pulling this file, change the key with yours, this is old mine since i have change it after commiting this file
     private static final String DARK_SKY_KEY = System.getenv("DARKSKY_KEY");
 
     private static final String DARK_SKY_URL = "https://api.darksky.net/forecast/";
